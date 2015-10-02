d55060e2aabbe7d2170b40e5382ff2916cd155c3
Sam Wang <samwang.com@gmail.com>
2015-10-02 12:31:53 +0800

    Initial Commit

---
+++ sparrow/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  sparrow
+//
+//  Created by Sam Wang on 15/10/2.
+//  Copyright © 2015年 Lanzisun Tech. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
